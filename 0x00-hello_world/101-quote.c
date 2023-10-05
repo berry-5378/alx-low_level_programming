7379cd6fdcb8971f35fbb8817eb0faa3df150c46
berry <you@example.com>
2023-10-05 13:58:01 -0700

    a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - is the function start and end place
+*Return: is to run acurretly
+*/
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
