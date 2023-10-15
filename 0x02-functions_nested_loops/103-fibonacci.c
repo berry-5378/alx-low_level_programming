28a72fbcf7b38b75f87137adaa9118674ce98bea
berry <you@example.com>
2023-10-15 03:39:08 -0700

    the sum of the even-valued terms followed by a new line

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n1 = 1, n2 = 2, next;
+	int s = 2;
+
+	while (n2 <= 4000000)
+	{
+next = n1 + n2;
+if (n2 % 2 == 0)
+{
+	s += n2;
+}
+n1 = n2;
+n2 = next;
+}
+printf("%d", s);
+printf("\n");
+return (0);
+}
