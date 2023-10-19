c09e534253ce16b94d3bb0991b96658d27c1c3b9
berry <you@example.com>
2023-10-19 05:21:51 -0700

    a function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+/**
+ * _strcmp - Prototype
+ * Return: On success
+ * @s2: function parameter
+ * @s1: function parameter
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
