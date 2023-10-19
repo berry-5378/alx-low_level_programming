7e817a8d2d4838851b05e987e37fa327d0e031c1
berry <you@example.com>
2023-10-19 03:21:03 -0700

    a function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,18 +7,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int k = -1;
+	int i = 0, j = 0;
 
-	for (i = 0 ; dest[i] != '\0' ; i++)
-		;
-
-	do
+	while (dest[i] != '\0')
 	{
-		k++;
-		dest[i] = src[k];
 		i++;
 	}
-	while (src[k] != 0);
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
